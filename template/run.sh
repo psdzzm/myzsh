bfd62b832a6b79e0ce4f61a469bccc08eb6ee2a6
Ethan Zhang <ethan.uni@gmx.com>
2023-07-09 23:53:59 +0800

    Add custom rust and fix matlab

--- template/run.sh
+++ template/run.sh
@@ -10,3 +10,5 @@ echo "SCRIPT_DIR: $SCRIPT_DIR"
 # link clangd configuration
 mkdir -p ~/.config/clangd
 ln -sf $SCRIPT_DIR/clangd.yaml ~/.config/clangd/config.yaml
+
+ln -sf $SCRIPT_DIR/java.opts ~/java.opts
