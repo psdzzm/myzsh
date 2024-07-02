c203a6d30ea6d1a5cc635af10c01540c409101b6
Ethan Zhang <ethan.uni@gmx.com>
2024-07-02 19:01:47 +0200

    Update Docker configuration and git settings
    - Added gitattributes file for handling PDFs.
    - Added a new file `docker-ramdisk.sh` with configuration for a docker ramdisk.
    - Set up environment variables and directory paths in `docker-ramdisk.sh`.
    - Checked if the script is running as root in `docker-ramdisk.sh`.
    - Created a bridge directory if it does not exist in `docker-ramdisk.sh`.
    - Created a base directory if it does not exist in `docker-ramdisk.sh`.
    - Started the Docker daemon with specific configurations in `docker-ramdisk.sh`.
    - Updated the comment character in the line with `D` to `#` in `ruff.toml`.
    - Removed the link to `vimrc` and added a link to firejail configuration in `run.sh`.
    - Renamed `template/vimrc` to `runcoms/vimrc`.
    - Set the default branch to `main` in `git init` in `gitconfig`.
    - Added `delta` as the pager for `git core` in `gitconfig`.
    - Added `delta` as the diff filter for `git interactive` in `gitconfig`.
    - Set `delta` options for `git delta` in `gitconfig`.
    - Set `diff3` as the conflict style for `git merge` in `gitconfig`.
    - Added `pdftotext` as the text converter for `git diff` with `pdf` files in `gitconfig`.
    - Added `hexdump` as the text converter for `git diff` with binary files in `gitconfig`.
    - Added `gh` as the credential helper for `github.com` and `gist.github.com` in `gitconfig`.
    - Added a check for Qt dependencies in `pinentry-auto`.
    - Added a check for GTK dependencies in `pinentry-auto`.
    - Executed `pinentry-qt` if Qt dependencies found in `pinentry-auto`.
    - Executed `pinentry-gtk-2` if GTK dependencies found in `pinentry-auto`.
    - Executed `pinentry-curses` as fallback in `pinentry-auto`.

---
+++ template/docker-ramdisk.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+BRIDAGE=docker_ramdisk
+BASE_DIR=/home/ethan/ramdisk/$BRIDAGE
+
+# Check if root
+if [ $(id -u) -ne 0 ]; then
+    echo "This script must be run as root"
+    exit 1
+fi
+
+# Create the bridge directory if it does not exist
+if ! ip link show $BRIDAGE ; then
+    ip link add name $BRIDAGE type bridge
+    ip addr add "10.20.30.1/24" dev $BRIDAGE
+    ip link set dev $BRIDAGE up
+fi
+
+# Create the base directory if it does not exist
+if [ ! -d $BASE_DIR ]; then
+    mkdir -p $BASE_DIR
+fi
+
+dockerd -H unix:///var/run/$BRIDAGE.sock \
+        -p /var/run/$BRIDAGE.pid \
+        --bridge=docker_ramdisk \
+        --config-file /home/ethan/ramdisk/docker.json \
+        --data-root=/home/ethan/ramdisk/$BRIDAGE \
+        --exec-root=/home/ethan/ramdisk/$BRIDAGE
